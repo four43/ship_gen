586a5da6bc67ca9e1f016787095c894b28f9f8c2
Seth Miller <seth@four43.com>
2021-11-14 19:59:13 -0600

    Refactor to remove loops, cache more values

--- src/rocket.rs
+++ src/rocket.rs
@@ -63,9 +63,104 @@ pub mod rocket {
     ];
 
     pub struct Rocket {
-        pub height: usize,
+        pub max_height: usize,
         pub max_width: usize,
+
         sections: Vec<&'static Part>,
+        height: usize,
+        bottom_width: usize,
+    }
+
+    impl Default for Rocket {
+        fn default() -> Self {
+            Rocket { max_height: 3, max_width: 3, sections: Vec::new(), height: 0, bottom_width: 0 }
+        }
+    }
+
+    impl Rocket {
+        pub fn new(max_height: usize) -> Rocket {
+            let mut rocket = Rocket {
+                max_height,
+                ..Rocket::default()
+            };
+            rocket.build();
+            return rocket;
+        }
+
+        fn append_section(&mut self, part: &'static Part) {
+            if part.height + self.height > self.max_height {
+                panic!("Cannot add part because it would make the rocket too tall")
+            }
+            self.sections.push(part);
+            self.height += part.height;
+            self.bottom_width = part.width;
+        }
+
+        fn prepend_section(&mut self, part: &'static Part) {
+            if part.height + self.height > self.max_height {
+                panic!("Cannot add part because it would make the rocket too tall")
+            }
+            self.sections.insert(0, part);
+            self.height += part.height;
+        }
+
+        fn part_height_remaining(&self) -> usize {
+            self.max_height - self.height
+        }
+
+        fn build(&mut self) {
+            if self.max_height < 3 {
+                panic!("Cannot build a rocket shorter than 3 sections")
+            }
+            let nose_cone = Rocket::choose_part(&PARTS_BIN, |x| { x.type_ == PartType::TRANSITION(0) });
+            self.append_section(nose_cone);
+
+            let mut rng = rand::thread_rng();
+            let body_decor_ratio = rng.gen_range(0.2..0.4);
+
+            // Add body or transition
+            while (self.part_height_remaining() as f32 / self.height as f32) > body_decor_ratio && self.part_height_remaining() > 2 {
+                let next_part = Rocket::choose_part(&PARTS_BIN, |x| {
+                    (x.type_ == PartType::TRANSITION(self.bottom_width)
+                        || (x.type_ == PartType::BODY && x.width == self.bottom_width))
+                        && x.height <= (self.part_height_remaining() - 2)
+                });
+                self.append_section(next_part);
+            }
+            // Finish up and add engine
+            let engine_part = Rocket::choose_part(&PARTS_BIN, |x| {
+                x.type_ == PartType::ENGINE(self.bottom_width)
+                    && x.height <= self.part_height_remaining()
+            });
+            self.append_section(engine_part);
+
+            // Add decoration (exhaust or nose)
+            while self.part_height_remaining() > 0 {
+                let decoration_part = Rocket::choose_part(&PARTS_BIN, |x| {
+                    (x.type_ == PartType::TIP
+                        || (x.type_ == PartType::EXHAUST(self.bottom_width)))
+                        && x.height <= self.part_height_remaining()
+                });
+                if decoration_part.type_ == PartType::TIP {
+                    self.prepend_section(decoration_part);
+                } else {
+                    self.append_section(decoration_part);
+                }
+            }
+        }
+
+        fn choose_part<P>(parts_list: &[Part], predicate: P) -> &Part
+            where
+                P: FnMut(&&Part) -> bool
+        {
+            let mut rng = rand::thread_rng();
+            let possible_parts = parts_list.iter().filter(predicate).collect::<Vec<&Part>>();
+
+            let dist = WeightedIndex::new(possible_parts.iter()
+                .map(|x| x.selection_weight)).unwrap();
+
+            possible_parts[dist.sample(&mut rng)]
+        }
     }
 
     impl fmt::Display for Rocket {
@@ -88,86 +183,6 @@ pub mod rocket {
                 }
             }
             write!(f, "{}", output)
-        }
-    }
-
-    impl Default for Rocket {
-        fn default() -> Self {
-            Rocket { height: 3, max_width: 3, sections: Vec::new() }
-        }
-    }
-
-    impl Rocket {
-        pub fn new(height: usize) -> Rocket {
-            let mut rocket = Rocket {
-                height,
-                ..Rocket::default()
-            };
-            rocket.build();
-            return rocket;
-        }
-
-        fn part_height_remaining(&self) -> usize {
-            let sections_size: usize = self.sections.iter().map(|x| x.height).sum();
-            return self.height - sections_size;
-        }
-
-        fn bottom_width(&self) -> usize {
-            match self.sections.last() {
-                None => 0,
-                Some(x) => x.width
-            }
-        }
-
-        fn build(&mut self) {
-            if self.height < 3 {
-                panic!("Cannot build a rocket shorter than 3 sections")
-            }
-            let nose_cone = Rocket::choose_part(&PARTS_BIN, |x| { x.type_ == PartType::TRANSITION(0) });
-            self.sections.push(nose_cone);
-
-            let mut rng = rand::thread_rng();
-            let body_decor_ratio = rng.gen_range(0.2..0.4);
-
-            // Add body or transition
-            while (self.part_height_remaining() as f32 / self.height as f32) > body_decor_ratio && self.part_height_remaining() > 2 {
-                let next_part = Rocket::choose_part(&PARTS_BIN, |x| {
-                    (x.type_ == PartType::TRANSITION(self.bottom_width())
-                        || (x.type_ == PartType::BODY && x.width == self.bottom_width()))
-                        && x.height <= (self.part_height_remaining() - 2)
-                });
-                self.sections.push(next_part);
-            }
-            // Finish up and add engine
-            let engine_part = Rocket::choose_part(&PARTS_BIN, |x| {
-                x.type_ == PartType::ENGINE(self.bottom_width()) && x.height <= self.part_height_remaining()
-            });
-            self.sections.push(engine_part);
-
-            // Add decoration (exhaust or nose)
-            while self.part_height_remaining() > 0 {
-                let decoration_part = Rocket::choose_part(&PARTS_BIN, |x| {
-                    (x.type_ == PartType::TIP || (x.type_ == PartType::EXHAUST(self.bottom_width()))) && x.height <= self.part_height_remaining()
-                });
-                if decoration_part.type_ == PartType::TIP {
-                    self.sections.insert(0, decoration_part)
-                } else {
-                    self.sections.push(decoration_part);
-                }
-            }
-        }
-
-        fn choose_part<P>(parts_list: &[Part], predicate: P) -> &Part
-            where
-                P: FnMut(&&Part) -> bool
-        {
-            let mut rng = rand::thread_rng();
-            let possible_parts = parts_list.iter().filter(predicate).collect::<Vec<&Part>>();
-
-            let dist = WeightedIndex::new(possible_parts.iter()
-                .map(|x| x.selection_weight)).unwrap();
-
-            possible_parts[dist.sample(&mut rng)]
         }
     }
 }
